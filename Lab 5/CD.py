1aeca3aae3cb7a6eb80ac74e6d165b403adb73f8
Brett Medjuck <brettmedjuck@cmail.carleton.ca>
2020-10-18 13:02:41 -0400

    Stuff

---
+++ Lab 5/CD.py
@@ -0,0 +1,8 @@
+# SYSC3010 Lab 5 - CD.py
+#     The file to be jointly edited by Team Members C and D
+
+from D import *
+
+def function_CD():
+    print ("CD")
+    function_D()
---
+++ Lab 5/D.py
@@ -0,0 +1,5 @@
+#SYSC3010 Lab 5 - D.py
+#File created by Shakir Cottle
+
+def function_D():
+    print("D")
